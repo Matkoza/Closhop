d5c1966da08e3d294be21290046d700ce2331bcb
matej <matej.mujezinovic@gmail.com>
2021-04-09 13:04:32 +0200

    added enviroment variable setup

--- api/config.php
+++ api/config.php
@@ -1,10 +1,21 @@
 <?php
 
 class Config {
-  const DB_HOST = "localhost";
-  const DB_USERNAME = "root";
-  const DB_PASSWORD = "rootroot";
-  const DB_SCHEME = "clothing-website";
+  public static function DB_HOST(){
+      return Config::get_env("DB_HOST", "localhost");
+    }
+    public static function DB_USERNAME(){
+      return Config::get_env("DB_USERNAME", "root");
+    }
+    public static function DB_PASSWORD(){
+      return Config::get_env("DB_PASSWORD", "");
+    }
+    public static function DB_SCHEME(){
+      return Config::get_env("DB_SCHEME", "clothing-website");
+    }
+    public static function DB_PORT(){
+      return Config::get_env("DB_PORT", "3306");
+    }
 
   const SMTP_HOST = "smtp.gmail.com";
   const SMTP_PORT = 587;
@@ -15,6 +26,12 @@ class Config {
   const DATE_FORMAT= "Y-m-d H:i:s";
 
   const JWT_SECRET="7hs9cDoesadjf92c2%2c3";
+
+  const JWT_TOKEN_TIME=604800;
+
+  public static function get_env($name, $default){
+  return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+}
 }
 
 ?>
